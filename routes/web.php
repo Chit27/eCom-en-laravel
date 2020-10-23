c2a6beab48d01dc5e19d729ad0aeec68d7b101ac
unknown <yadanawin1@gmail.com>
2020-10-23 17:41:57 +0630

    cart product listing

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::get("/",[ProductController::class,'index']);
 Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::get("search",[ProductController::class,'search']);
 Route::post("add_to_card",[ProductController::class,'addToCard']);
+Route::get("cartlist",[ProductController::class,'cartList']);
